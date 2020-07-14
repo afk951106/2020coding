957586a85a6ce6cf96f4a702a7fca0fc7b231562
unknown <afk951106>
2020-07-14 22:03:19 +0800

    0714

--- POSTtest02.php
+++ POSTtest02.php
@@ -1,13 +1,36 @@
 <?php
 //post
+if ($_POST["User"] == NULL )
+{
+  print "請輸入帳號";
+}
+if($_POST["Pass"] == NULL)
+{
+    print "請輸入密碼";
+}
+
 $username = $_POST["User"];
 $userpassword = $_POST["Pass"];
+$userphone = $_POST["choicephone"];
 
+switch($userphone){
+    case "iphone":
+        print "你的手機是IPHONE"; 
+    break;
+     case "htc":
+        print "你的手機是HTC";
+     break;
+    case "sony":
+        print "你的手機是SONY";
+    break;
+
+}
 if ($username == "hello" and $userpassword == "0000"){
 
     print "yourName is $username</br>";
     print "yourPassword is $userpassword<br>";
 }
 else{
-    print "Login fault";}
+    print "Login fault";
+}
 ?>
